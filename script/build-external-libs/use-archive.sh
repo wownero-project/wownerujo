66075229bcb1da5705016df1bdba5e4cb237d945
fuwa <echowss@gmail.com>
2018-07-31 20:39:25 +0000

    update external lib version

--- script/build-external-libs/use-archive.sh
+++ script/build-external-libs/use-archive.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-version=v1.5.10.4
-rm -rf external-libs
+version=v1.5.10.5-alpha
+
 curl -O -L https://github.com/fuwa0529/wownerujo/releases/download/${version}/external-libs.tgz
+
+rm -rf external-libs
 tar zxfv external-libs.tgz
 rm external-libs.tgz
