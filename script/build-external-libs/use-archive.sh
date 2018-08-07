42e889f851353804bcbbdb84ab6039121ae521ac
fuwa <echowss@gmail.com>
2018-08-07 13:06:13 +0000

    add echo

--- script/build-external-libs/use-archive.sh
+++ script/build-external-libs/use-archive.sh
@@ -7,5 +7,7 @@ version=v1.5.10.5
 curl -O -L https://github.com/fuwa0529/wownerujo/releases/download/${version}/external-libs.tgz
 
 rm -rf external-libs
+
+echo "unpacking external-libs"
 tar zxfv external-libs.tgz
 rm external-libs.tgz
