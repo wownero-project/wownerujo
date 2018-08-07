834827c6591f3f53b845781a97772feadf42338d
fuwa <echowss@gmail.com>
2018-08-07 03:12:37 +0000

    prep release

--- script/build-external-libs/fetch-wownero.sh
+++ script/build-external-libs/fetch-wownero.sh
@@ -6,11 +6,10 @@ source script/build-external-libs/env.sh
 
 cd $EXTERNAL_LIBS_BUILD_ROOT
 
-version="d4ee3c3"
+version="v0.2.2.0"
 
 rm -rf wownero
 git clone https://github.com/wownero/wownero
 cd wownero
 
 git checkout $version
-git submodule init && git submodule update
