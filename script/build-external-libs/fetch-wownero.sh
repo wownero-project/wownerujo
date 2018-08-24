5af919883731a5963a78df82bcbde4b1ac8394b2
fuwa <echowss@gmail.com>
2018-08-24 09:15:22 +0000

    use ringsize 22 wallet-api

--- script/build-external-libs/fetch-wownero.sh
+++ script/build-external-libs/fetch-wownero.sh
@@ -6,7 +6,10 @@ source script/build-external-libs/env.sh
 
 cd $EXTERNAL_LIBS_BUILD_ROOT
 
-version="v0.2.2.0"
+# version="v0.2.2.0"
+
+# branch-release-v0.3.0.0 (ring size 22)
+version="48bcb9"
 
 rm -rf wownero
 git clone https://github.com/wownero/wownero
