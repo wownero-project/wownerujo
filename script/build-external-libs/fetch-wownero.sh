d356be8c1810b77bfe0284bb39624d83b5a25897
fuwa <echowss@gmail.com>
2018-09-15 07:52:48 +0800

    test wownero3

--- script/build-external-libs/fetch-wownero.sh
+++ script/build-external-libs/fetch-wownero.sh
@@ -8,11 +8,15 @@ cd $EXTERNAL_LIBS_BUILD_ROOT
 
 # version="v0.2.2.0"
 
+# url="https://github.com/wownero/wownero"
 # branch-release-v0.3.0.0 (ring size 22)
-version="48bcb9"
+# version="48bcb9"
+
+url="https://github.com/wowario/wownero3"
+version="61753be"
 
 rm -rf wownero
-git clone https://github.com/wownero/wownero
+git clone $url wownero
 cd wownero
 
 git checkout $version
