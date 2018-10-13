48b9dd8ef67540304643cd89efce097fbc0fa958
fuwa <echowss@gmail.com>
2018-10-13 20:40:57 +0800

    prep 2.3.0.2

--- script/nix/shell.nix
+++ script/nix/shell.nix
@@ -96,7 +96,7 @@ with nixpkgs;
     export _JAVA_AWT_WM_NONREPARENTING=1
 
     export app_version=1110
-    export gradle_app_version=2x3x1x0
+    export gradle_app_version=2x3x0x2
     export release_key=~/.android/fuwa-release-key.jks
 
     export USE_CCACHE=1
