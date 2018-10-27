6d7433606bb9f0e048f94a77067d7c95928c20fc
fuwa <echowss@gmail.com>
2018-10-27 10:32:56 +0000

    update wownero rev

--- script/build-external-libs/fetch-wownero.sh
+++ script/build-external-libs/fetch-wownero.sh
@@ -10,10 +10,10 @@ cd $EXTERNAL_LIBS_BUILD_ROOT
 
 
 # url="$base_dir/vendor/wownero"
-url="https://github.com/wownero/wownero"
-# url="https://github.com/wowario/wownero"
+# url="https://github.com/wownero/wownero"
+url="https://github.com/wowario/wownero"
 # version="v0.3.0.0"
-version="c929c2a"
+version="0a97b3"
 
 
 rm -rf wownero
