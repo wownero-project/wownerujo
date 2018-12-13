d71247c88aa06f32495b4805a8ac847b8c3a3bfb
fuwa <fuwa25519@protonmail.com>
2018-12-13 22:17:39 +0800

    update ver

--- script/build-external-libs/fetch-wownero.sh
+++ script/build-external-libs/fetch-wownero.sh
@@ -10,10 +10,10 @@ cd $EXTERNAL_LIBS_BUILD_ROOT
 
 
 # url="$base_dir/vendor/wownero"
-url="https://github.com/wownero/wownero"
-# url="https://github.com/wowario/wownero"
-version="v0.4.0.0"
-# version="0a97b3"
+# url="https://github.com/wownero/wownero"
+url="https://github.com/wowario/wownero"
+# version="v0.4.0.0"
+version="df3b8a8"
 
 
 rm -rf wownero
