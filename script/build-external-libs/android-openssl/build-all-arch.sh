05aa2e4594ddf91f2594d45c368515fa9a271a9a
fuwa <fuwa25519@protonmail.com>
2018-12-25 19:10:31 +0800

    echo arch

--- script/build-external-libs/android-openssl/build-all-arch.sh
+++ script/build-external-libs/android-openssl/build-all-arch.sh
@@ -61,7 +61,7 @@ for arch in ${archs[@]}; do
     perl -pi -e 's/SHLIB_MAJOR=1/SHLIB_MAJOR=`/g' Makefile
     perl -pi -e 's/SHLIB_MINOR=0.0/SHLIB_MINOR=`/g' Makefile
     make clean
-    echo "silent building openssl..."
+    echo "building openssl for ${arch}..."
     make depend -j $NPROC > /dev/null 2>&1
     make all -j $NPROC > /dev/null 2>&1
 
