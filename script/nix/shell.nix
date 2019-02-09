fe8a0df228954b0d7f0918b4eddc8c679e2d070d
fuwa <fuwa25519@protonmail.com>
2019-02-09 17:34:03 +0800

    bump version

--- script/nix/shell.nix
+++ script/nix/shell.nix
@@ -96,8 +96,8 @@ with nixpkgs;
 
     export _JAVA_AWT_WM_NONREPARENTING=1
 
-    export app_version=1150
-    export gradle_app_version=2x5x0x1
+    export app_version=1160
+    export gradle_app_version=2x5x0x2
     export release_key=~/.android/fuwa-release-key.jks
 
     export USE_CCACHE=1
